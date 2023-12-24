ff1f5a9a972d68a0644934e392aa233ed4c0bd10
metaproph3t <metaproph3t@protonmail.com>
2023-12-24 00:00:00 +0000

    Swap out anchor test for bankrun

--- tests/picobook.ts
+++ tests/picobook.ts
@@ -1,21 +1,33 @@
 import * as anchor from "@coral-xyz/anchor";
+import { BankrunProvider } from "anchor-bankrun";
+import { startAnchor } from "solana-bankrun";
 import { Program } from "@coral-xyz/anchor";
 import { Picobook } from "../target/types/picobook";
+const PicobookIDL: Picobook = require("../target/idl/picobook.json");
 
-describe("picobook", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const PICOBOOK_PROGRAM_ID = new anchor.web3.PublicKey(
+  "Cm1pi42PSyi6sVRMUJvaLyBGtU9cBNw4ciXCxNr1UPn5"
+);
 
-  const program = anchor.workspace.Picobook as Program<Picobook>;
+describe("picobook", async () => {
+  let context,
+    program;
+
+  before(async () => {
+    context = await startAnchor("./", [], []);
+    const provider = new BankrunProvider(context);
+    anchor.setProvider(provider);
+
+    program = new anchor.Program<Picobook>(
+     PicobookIDL,
+     PICOBOOK_PROGRAM_ID,
+     provider
+    );
+  });
 
   it("Is initialized!", async () => {
     // Add your test here.
-    try {
-      const tx = await program.methods.initialize().rpc();
-      console.log("Your transaction signature", tx);
-    } catch (err) {
-      console.error(err);
-    }
-
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
   });
 });
